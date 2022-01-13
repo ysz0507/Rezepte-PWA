fb5a9a20dc101c1f11d07ac090e5e30ef7552353
Yves-Simon Zeulner <ysz@web.de>
2022-01-13 01:16:53 +0100

    Installable

---
+++ js/rezepteHinzufuegen.js
@@ -0,0 +1,32 @@
+function addElementsByArray(arr, id, cssClass){
+
+    localStorage.setItem("selected", "");
+    var tableBody = document.createElement('tbody');
+    arr.forEach(function(name) {
+        var row = document.createElement('tr');
+        var cell = document.createElement('td');
+        cell.className = cssClass;
+        cell.addEventListener("click", function() {
+            rezeptAnzeigen(name);
+        });
+        cell.appendChild(document.createTextNode(name));
+        row.appendChild(cell);
+        tableBody.appendChild(row);
+    });
+    document.getElementById(id).appendChild(tableBody);
+
+    function rezeptAnzeigen(key){
+        console.log(key);
+        localStorage.setItem("selected", key);
+        window.location = '/html/rezept.html';
+    };
+}
+
+function rezeptLöschen(name){
+    var rezepte = JSON.parse(localStorage.getItem("Keys") || "[]");
+    if(rezepte.indexOf(name) == -1)
+        return;
+    rezepte.splice(rezepte.indexOf(name), 1);
+    localStorage.setItem("Keys", JSON.stringify(rezepte));
+    localStorage.removeItem(name);
+}
---
+++ sw.js
@@ -0,0 +1,38 @@
+console.log('I am a Service Worker!');
+
+var cacheName = 'CacheFor PWA';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/html/eintragen.html',
+  '/html/rezept.html',
+  '/html/settings.html',
+  '/html/suche.html',
+  '/html/uebersicht.html',
+  '/css/main.css',
+  '/css/units.css',
+  '/js/main.js',
+  '/js/rezepteHinzufuegen.js',
+  '/images/cross.png',
+  '/images/gear.png',
+  '/images/search.png',
+  '/images/stack.png'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
